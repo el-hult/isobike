f56fc481328940030903aed1de409cab4e53687d
Ludvig Hult <ludvig.hult@gmail.com>
2015-07-11 00:40:56 +0200

    Added new usage of the fetcher file yielding the tsv2geojson file obsolete. Now the output storage format is much better!
    Time to look for the contours! :D

--- fetcher.py
+++ fetcher.py
@@ -7,16 +7,6 @@ import json
 import codecs
 import time
 
-## Setup
-n = 50
-lat1 = 59.29
-lat2 = 59.40
-long1  = 17.96
-long2  = 18.15
-delta_lat = (lat2-lat1)/(n-1)
-delta_long = (long2-long1)/(n-1)
-
-
 ## Defining exceptions for easier troubleshooting.
 class APIError(Exception):
      def __init__(self, value):
@@ -25,59 +15,135 @@ class APIError(Exception):
          return repr(self.value)
 
 
-## Write file headers
-with codecs.open("bike_times.tsv", encoding='utf-8', mode="w") as f:
-    s = '\t'.join(['node_id','long','lat','travel time[s]','Address name used in travel time'])
-    f.write(s)
+def fetch(n,origin,filename,format):
 
-f = open('bike_times.tsv','w')
+    lat1 = 59.29
+    lat2 = 59.40
+    long1  = 17.96
+    long2  = 18.15
+    delta_lat = (lat2-lat1)/(n-1)
+    delta_long = (long2-long1)/(n-1)
 
-tic = time.time()
-for current_node_id in range(1,n*n+1):
-    toc =  time.time() - tic
-    i =  (current_node_id-1) % n
-    j =  (current_node_id-1) // n
-    print current_node_id,toc,i,j
-    
-    p1 = (lat1 +j*delta_lat,long1+i*delta_long)
+    if format == 'tsv':
 
-    while True:
-        get_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=Acando, Malmskillnadsgatan, Stockholm&destinations='+str(p1[0])+','+str(p1[1])+'&mode=bicycling'
-        r=requests.get(get_url)
-        content = r.json()
-        
-        
-        if content['status'] == 'OK':
-            # print content
-            # print '1'
-            if content['rows'][0]['elements'][0]['status'] == 'OK':
-                try:
-                    bike_time_in_seconds = content['rows'][0]['elements'][0]['duration']['value']
-                    final_address = content['destination_addresses'][0]
-                    with codecs.open("bike_times.tsv", encoding='utf-8', mode="a") as f:
-                        s = '\t'.join([ str(current_node_id),\
-                            str(p1[0]), str(p1[1]), \
-                            str(bike_time_in_seconds), final_address])+'\n'
-                        f.write(s)
-                except KeyError:
-                    raise APIError('The JSON had a strange key-set. Result:\n'+content)
-            elif content['rows'][0]['elements'][0]['status'] == 'ZERO_RESULTS':
-                bike_time_in_seconds = 'N/A'
-                final_address = 'N/A'
-                with codecs.open("bike_times.tsv", encoding='utf-8', mode="a") as f:
-                    s = '\t'.join([ str(current_node_id),\
-                        str(p1[0]), str(p1[1]), \
-                        str(bike_time_in_seconds), final_address])+'\n'
-                    f.write(s)
-            break
-        elif content['status'] == 'OVER_QUERY_LIMIT':
-            print 'made an over-query limit... retrying in 10 seconds'
-            time.sleep(10)
-            continue
-        else:
-            # print 'Unknown',content
-            raise APIError('Unknown querey status. Result:\n'+content)
-            # print '3'
+        with codecs.open(filename+'.tsv', encoding='utf-8', mode="w") as output_file:
+            header = '\t'.join(['node_id','long','lat','travel time[s]','Address name used in travel time'])
+            output_file.write(header)
+
+        for current_node_id in range(1,n*n+1):
+            i =  (current_node_id-1) % n
+            j =  (current_node_id-1) // n
+            
+            destination_coordinates = (lat1 +j*delta_lat,long1+i*delta_long)
+
+            while True:
+                get_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins='+origin+'&destinations='+str(destination_coordinates[0])+','+str(destination_coordinates[1])+'&mode=bicycling'
+                r=requests.get(get_url)
+                content = r.json()
+                
+                
+                if content['status'] == 'OK':
+
+                    if content['rows'][0]['elements'][0]['status'] == 'OK':
+                        try:
+                            bike_time_in_seconds = content['rows'][0]['elements'][0]['duration']['value']
+                            final_address = content['destination_addresses'][0]
+                            with codecs.open(filename+'.tsv', encoding='utf-8', mode="a") as output_file:
+                                s = '\t'.join([ str(current_node_id),\
+                                    str(destination_coordinates[0]), str(destination_coordinates[1]), \
+                                    str(bike_time_in_seconds), final_address])+'\n'
+                                output_file.write(s)
+                        except KeyError:
+                            raise APIError('The JSON had a strange key-set. Result:\n'+content)
 
 
+                    elif content['rows'][0]['elements'][0]['status'] == 'ZERO_RESULTS':
+                        bike_time_in_seconds = 'N/A'
+                        final_address = 'N/A'
+                        with codecs.open(filename+'.tsv', encoding='utf-8', mode="a") as output_file:
+                            s = '\t'.join([ str(current_node_id),\
+                                str(destination_coordinates[0]), str(destination_coordinates[1]), \
+                                str(bike_time_in_seconds), final_address])+'\n'
+                            output_file.write(s)
+                    break
 
+                elif content['status'] == 'OVER_QUERY_LIMIT':
+                    print 'made an over-query limit... retrying in 10 seconds'
+                    time.sleep(10)
+                    continue
+
+                else:
+                    raise APIError('Unknown querey status. Result:\n'+content)
+
+    elif format == 'geojson':
+        json_object = {};
+        features = []
+
+        for current_node_id in range(1,n*n+1):
+            i =  (current_node_id-1) % n
+            j =  (current_node_id-1) // n
+            print 'i j n = 'i,j,n
+            
+            destination_lat  = lat1 +i*delta_lat
+            destination_long = long1+j*delta_long
+
+            while True:
+                get_url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'+\
+                    'origins='+origin+\
+                    '&destinations='+str(destination_lat)+','+str(destination_long)+\
+                    '&mode=bicycling'
+                r=requests.get(get_url)
+                content = r.json()
+                
+                
+                if content['status'] == 'OK':
+
+                    if content['rows'][0]['elements'][0]['status'] == 'OK':
+                        biking_time = content['rows'][0]['elements'][0]['duration']['value']
+                        final_address = content['destination_addresses'][0]
+                    elif content['rows'][0]['elements'][0]['status'] == 'ZERO_RESULTS':
+                        biking_time = -1
+                        final_address = 'N/A'
+                    else:
+                        raise NotImplementedError('This element status is not handled. Result:\n'+content)
+
+                    geom = {\
+                        'type': 'Point', \
+                        'coordinates': [destination_lat,\
+                                        destination_long]}
+                    props = {\
+                        'Destination name': final_address, \
+                        'Biking time [s]': biking_time,\
+                        'Longitude index': i,\
+                        'Latitude index': j}
+
+                    feature = dict(\
+                        type = 'Feature', \
+                        geometry = geom, \
+                        properties = props )
+
+                    features.append(feature)
+                    break
+
+                elif content['status'] == 'OVER_QUERY_LIMIT':
+                    print 'made an over-query limit... retrying in 10 seconds'
+                    time.sleep(10)
+                    continue
+
+                else:
+                    raise APIError('Unknown querey status. Result:\n'+content)
+
+        json_object = {\
+            'features': features,\
+            'type': "FeatureCollection"}
+
+        with codecs.open(filename+'.geojson', encoding='utf-8',mode='w') as output_file:
+            json.dump(json_object,output_file,indent=1,encoding="utf-8",ensure_ascii=False)
+
+    else:
+        raise SyntaxError('The only allowed formats are tsv and geojson!')
+
+
+## Setup
+n = 3
+fetch(n,'Acando, Malmskillnadsgatan, Stockholm','bike_times','geojson')
