0c10b996d69ecbfe3e74124feb5f8e418c26827d
Ludvig Hult <ludvig.hult@gmail.com>
2015-07-18 13:55:52 +0200

    Create polygonss where applicable, but interior polygons and plygon groups are still not handeled correctly. In the future, i might also make polygons out of the not-connected linestrings. Also, for the future, one should cosider a smoothing factor in the calculations to compensate for errors in the in-data.

--- curvefinder.py
+++ curvefinder.py
@@ -74,7 +74,7 @@ def curvefinder(array,level):
 
 	contours = skimage.measure.find_contours(time_mat,\
 		level, \
-		fully_connected='high', \
+		fully_connected='low', \
 		positive_orientation='low')
 		# connected=high => assume inner is fully connecter
 		#orientation=low => contour winds CCW around lower-than-level value
@@ -82,23 +82,29 @@ def curvefinder(array,level):
 	features = []
 	for n,contour in enumerate(contours) :
 
-
 		# longitudes
+		long_int_range = numpy.arange(len(array['Longitudes']))
 		longitudes_to_interpolate = contour[:,1]
-		y_vec = array['Longitudes']
-		x_vec = numpy.linspace(0,len(y_vec)-1,len(y_vec))
-		longtitudes_interpolated = numpy.interp(longitudes_to_interpolate,x_vec,y_vec)
+		longitudes_interpolated = numpy.interp(longitudes_to_interpolate,long_int_range,array['Longitudes'])
 		
 		# latitudes
+		lat_int_range = numpy.arange(len(array['Latitudes']))
 		latitudes_to_interpolate = contour[:,0]
-		y_vec = array['Latitudes']
-		x_vec = numpy.linspace(0,len(y_vec)-1,len(y_vec))
-		latitudes_interpolated = numpy.interp(latitudes_to_interpolate,x_vec,y_vec)
+		latitudes_interpolated = numpy.interp(latitudes_to_interpolate,lat_int_range,array['Latitudes'])
 
-		coordinates = numpy.vstack((longtitudes_interpolated,latitudes_interpolated))
-		
-		geom = {'type': 'LineString','coordinates': numpy.transpose(coordinates).tolist()}
-		props = {'Travel time [s]': level}
+		coordinates = numpy.vstack((longitudes_interpolated,latitudes_interpolated))
+		coordinates = numpy.transpose(coordinates).tolist()
+
+		# check if it bites its tail!
+		if coordinates[0] == coordinates[-1]:
+			feature_type = 'Polygon'
+			coordinates_bundle = [ coordinates ] # polygons need extra 'wrapping'
+		else:
+			feature_type = 'LineString'
+			coordinates_bundle = coordinates
+
+		geom = {'type': feature_type,'coordinates': coordinates_bundle}
+		props = {'Biking time [s]': level}
   		feature = {'type':'Feature','geometry':geom,'properties': props}
   		features.append(feature)
 	return features
@@ -109,6 +115,7 @@ def read_array(filename):
 	if not(os.path.isfile(filename)):
 		raise InputDataError('read_array','File '+filename+'doesn\'t exist.')
 
+	# check the file ending
 	filename_prefix, filename_suffix = os.path.splitext(filename)
 	accepted_file_endings=['.geojson', '.gjson', '.json']
 	if not(filename_suffix in accepted_file_endings):
@@ -118,8 +125,8 @@ def read_array(filename):
 	with codecs.open(filename, encoding='utf-8', mode="r") as input_file:
 		input_json = json.load(input_file)
 
+	# validate the GeoJSON
 	r = requests.post("http://geojsonlint.com/validate", data=json.dumps(input_json))
-
 	if r.json()['status'] == 'error':
 		if r.json()['message'] == 'Data was not JSON serializeable.':
 			raise StandardError('Something went wrong.')
@@ -127,42 +134,42 @@ def read_array(filename):
 			error_message = r.json()['status']
 			raise InputDataError('read_array','Data failed GeoJSON validation:'+error_message)
 
-	# create long- and lat- and time- matrices
+	# validate the number of data points and allocate some space
 	features = input_json['features']
-	number_of_features = len(features)
-
-	n_float = math.sqrt(number_of_features)
+	n_float = math.sqrt(len(features))
 	n = int(n_float)
 	if not(n_float == n):
 		raise InputDataError('read_array','The data was not a perfect square!')
-
-	long_min = 180
-	long_max = -180
-	lat_min = 180
-	lat_max = -180
-
+	long_min = lat_min = 180
+	long_max = lat_max = -180
 	time_mat = numpy.empty(shape=(n,n))
-	bajs = 1
+
+
+
 	for feature in features:
-		x_i = feature['properties']['Latitude index']
-		y_i = feature['properties']['Longitude index']
+
+		y_i = feature['properties']['Latitude index']
+		x_i = feature['properties']['Longitude index']
 		travel_time = feature['properties']['Biking time [s]']
 		latitude = feature['geometry']['coordinates'][1]
 		longitude = feature['geometry']['coordinates'][0]
+
 		lat_max = max(latitude,lat_max)
 		long_max = max(longitude,long_max)
 		lat_min = min(latitude,lat_min)
 		long_min = min(longitude,long_min)
+
 		if travel_time == -1:
 			travel_time = numpy.nan
+
 		time_mat[x_i,y_i] = travel_time
 
 		
 
+	# return the matrices
 	
 	long_list = numpy.linspace(long_min,long_max,n)
 	lat_list = numpy.linspace(lat_min,lat_max,n)
-	# return the matrices
 	data = {\
 		'Longitudes':long_list,\
 		'Latitudes': lat_list,\
@@ -171,17 +178,14 @@ def read_array(filename):
 
 if __name__ == '__main__':
 	
-	n=43
+	n=50
 
 	# call readarray on the relevant file
 	data = read_array(\
 		'bike_times_'+str(n)+'.geojson')
 	
-	levels = [3, 5, 7, 10, 20, 30]
-	#levels = [3]
-	levels[:] = [x*60 for x in levels] 
-
 	# call the curvesfinder with relevant values
+	levels = [5*60, 10*60, 15*60, 20*60, 25*60, 30*60]
 	level_set_geojson = curvesfinder(data,levels)
 
 	with codecs.open('level_sets_'+str(n)+'.geojson', encoding='utf-8',mode='w') as output_file:
